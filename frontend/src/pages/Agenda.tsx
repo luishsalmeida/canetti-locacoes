7970baaaaa12d567648dd5af9983a6c136ffd2d0
luishsalmeida <luishsalmeida@gmail.com>
2026-09-01 11:30:54 -0300

    Permitir valores decimais nos disparos

--- frontend/src/pages/Agenda.tsx
+++ frontend/src/pages/Agenda.tsx
@@ -578,7 +578,7 @@ export const Agenda: React.FC = () => {
                             required
                           />
                           {tiposDisparoDoEquipamento(eq).map((tipo) => (
-                            <Input key={tipo} label={`${tipo} (un.)`} placeholder="Quantidade" type="number" min="0" step="1"
+                            <Input key={tipo} label={`${tipo} (un.)`} placeholder="Quantidade" type="number" min="0" 
                               value={itemEncontrado.valoresDisparo?.[tipo] ?? ''}
                               onChange={(e) => handleValorDisparoChange(eq.id, tipo, Number(e.target.value))} />
                           ))}
